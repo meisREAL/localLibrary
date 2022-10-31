434973ae8879ecf5e667a578c8e4b5501091b4d6
meisReal <simas.cesnakauskas@gmail.com>
2022-10-31 09:34:56 +0200

    show single book information

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,7 +4,7 @@ const Genre = require('../models/genre');
 const BookInstance = require('../models/bookinstance');
 
 const async = require('async');
-
+// display index page
 exports.index = (req, res) => {
     async.parallel(
         {
@@ -49,8 +49,38 @@ exports.book_list = function (req, res, next) {
 };
 
 //* Display detail page for a specific book.
-exports.book_detail = (req, res) => {
-    res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+exports.book_detail = (req, res, next) => {
+    async.parallel(
+        {
+            book(callback){
+                Book.findById(req.params.id)
+                    .populate('author')
+                    .populate('genre')
+                    .exec(callback)
+            },
+            book_instance(callback){
+                BookInstance.find({book: req.params.id})
+                    .exec(callback);
+            },
+        },
+        (err, results) =>{
+            if(err){
+                return next(err);
+            }
+            if(results.book == null){
+                //No results of the book
+                const err = new Error('Book not found');
+                err.status = 404;
+                return next(err);
+            }
+            // Successful, so render.
+            res.render('book_detail',{
+                title: results.book.title,
+                book: results.book,
+                book_instance: results.book_instance,
+            });
+        }
+    );
 };
 
 //* Display book create form on GET.
