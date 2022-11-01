3731d6458c3c45e23a13eb9b44d3a8ff7f7a741c
meisReal <simas.cesnakauskas@gmail.com>
2022-11-01 12:20:45 +0200

    ability to delte book instance

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,6 +1,7 @@
 const BookInstance = require('../models/bookinstance');
 const {body, validationResult} = require('express-validator');
-const Book = require('../models/book')
+const Book = require('../models/book');
+const async = require('async');
 
 //* Display list of all BookInstances.
 exports.bookinstance_list = function (req, res, next) {
@@ -110,13 +111,50 @@ exports.bookinstance_create_post = (req, res, next) => [
 ];
 
 //* Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = (req, res, next) => {
+    async.parallel(
+        {
+            bookinstance(callback){
+                BookInstance.findById(req.params.id).exec(callback);
+            },
+        },
+        (err, results)=>{
+            if(err){
+                return next(err);
+            }
+            if(results.bookinstance == null){
+                //No results
+                res.redirect('/catalog/bookinstances');
+            }
+            // Successful, so render
+            res.render('bookinstance_delete', {
+                title: 'Delete Book Instance',
+                bookinstance: results.bookinstance,
+            });
+        }
+    );
 };
 
 //* Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = (req, res) => {
-    res.send("NOT IMPLEMENTED: BookInstance delete POST");
+exports.bookinstance_delete_post = (req, res, next) => {
+    async.parallel(
+        {
+            bookinstance(callback){
+                BookInstance.findById(req.body.bookinstanceid).exec(callback);
+            },
+        },
+        (err, results) => {
+            if(err){
+                return next(err);
+            }
+            BookInstance.findByIdAndRemove(req.body.bookinstanceid, (err) => {
+                if(err){
+                    return next(err);
+                }
+                res.redirect('/catalog/bookinstances')
+            })
+        }
+    );
 };
 
 //* Display BookInstance update form on GET.
